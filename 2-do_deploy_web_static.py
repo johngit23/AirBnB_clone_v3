0b9ae12ed61fe0f86ec7831a227ceb549f038d15
johngit23 <126653377+johngit23@users.noreply.github.com>
2023-10-04 21:54:45 +0300

    do deploy update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,48 +1,48 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
-import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+# a Fabric script that distributes an archive to your web servers
 
-env.hosts = ["100.25.129.130", "35.153.50.107"]
+from fabric.api import env, put, run
+import os
+
+env.hosts = ['100.25.129.130', '35.153.50.107']
+env.key_filename = "~/.ssh/holberton"
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
+    ''' distributes an archive to your web servers '''
     if os.path.isfile(archive_path) is False:
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
-
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
+    put(archive_path, "/tmp")
+    file_split = archive_path.split('/')
+    file_w_ext = file_split[1]
+    file_wo_ext = file_split[1].split('.')[0]
     if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
+           format(file_wo_ext)).failed is True:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
+    print("Release folder create done!")
+    if run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".
+           format(file_wo_ext, file_wo_ext)).failed is True:
         return False
-    if run("rm /tmp/{}".format(file)).failed is True:
+    print("Uncompress the archive done!")
+    if run("rm /tmp/{}".format(file_w_ext)).failed is True:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+    print("Delete the archive done!")
+    if run("mv /data/web_static/releases/{}/web_static/*"
+           " /data/web_static/releases/{}/".
+           format(file_wo_ext, file_wo_ext)).failed is True:
         return False
+    print("Move all files and folders to one directory done!")
     if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
+           format(file_wo_ext)).failed is True:
         return False
+    print("Delete empty folder web_static done!")
     if run("rm -rf /data/web_static/current").failed is True:
         return False
+    print("Delete the symbolic link done!")
     if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
+           format(file_wo_ext)).failed is True:
         return False
+    print("Created new symbolic link done!")
+    print("New version deployed!")
     return True
