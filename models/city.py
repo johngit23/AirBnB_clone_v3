98a520f509cba7befba5df0eb8ebe34b017d481e
johngit23 <126653377+johngit23@users.noreply.github.com>
2023-10-28 10:57:26 +0300

    update on api

---
+++ models/city.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+""" holds class City"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class City(BaseModel, Base):
+    """Representation of city """
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != 'db':
+        @property
+        def places():
+            """getter for list of places related to city"""
+            place_list = []
+            all_places = models.storage.all(Place)
+            for place in all_places.values():
+                if place_list.id == self.id:
+                    place_list.append(place)
+            return place_list
